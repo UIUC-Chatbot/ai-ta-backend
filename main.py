8c752d6e95b9e918252883267135b1adde7297c5
Kastan Day <kastanvday@gmail.com>
2023-05-15 15:54:11 -0500

    adding /getContexts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
 import os
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/getContexts')
+def getContexts():
+    language = request.args.get('language')
+    return jsonify({"language": f"You said: {language}"})
+
 
 
 if __name__ == '__main__':
