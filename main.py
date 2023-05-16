924f14afc025528ada9568b8d8f1194632f9cd85
Kastan Day <kastanvday@gmail.com>
2023-05-16 09:49:16 -0500

    adding CORS access control

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from qdrant_client import QdrantClient
 
 app = Flask(__name__)
+CORS(app)
 
+# load API keys from globally-availabe .env file
+load_dotenv(dotenv_path='.env', override=True)
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-@app.route('/getContexts')
+@app.route('/getContexts', methods=['GET'])
 def getContexts():
     language = request.args.get('language')
-    return jsonify({"language": f"You said: {language}"})
+    response = jsonify({"language": f"You said: {language}"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
+
+def get_contexts():
+    contexts = {'language': 'python', 'framework': 'Flask'}
+    response = jsonify(contexts)
+    return response
+
+
+
+@app.route('/getqdrant')
+def getqdrant():
+    qdrant_client = QdrantClient(
+        url=os.environ.get("QDRANT_URL"),
+        api_key=os.environ.get("QDRANT_API_KEY"),
+    )
+    return 'Placeholder return'
 
 
 if __name__ == '__main__':
