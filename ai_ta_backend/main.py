2b0868e6257966a479dd85a6dfc7581ade887e77
Kastan Day <kastanvday@gmail.com>
2023-06-14 20:53:17 -0700

    adding prints to stuffed prompt

--- ai_ta_backend/main.py
+++ ai_ta_backend/main.py
@@ -139,16 +139,21 @@ def getContextStuffedPrompt():
     a very long "stuffed prompt" with question + summaries of 20 most relevant documents.
   """
   print("In /getContextStuffedPrompt")
-  
 
   ingester = Ingest()
   search_query: List[str] | str = request.args.get('search_query')
   course_name: List[str] | str = request.args.get('course_name')
   top_n: int = int(request.args.get('top_n'))
   top_k_to_search: int = int(request.args.get('top_k_to_search'))
+  # print all input params
+  print(f"search_query: {search_query}")
+  print(f"course_name: {course_name}")
+  print(f"top_n: {top_n}")
+  print(f"top_k_to_search: {top_k_to_search}")
 
   start_time = time.monotonic()
   stuffed_prompt = ingester.get_context_stuffed_prompt(search_query, course_name, top_n, top_k_to_search)
+  print("stuffed_prompt: ", stuffed_prompt)
   print(f"⏰ Runtime of EXTREME prompt stuffing: {(time.monotonic() - start_time):.2f} seconds")
   response = jsonify({"prompt": stuffed_prompt})
 
