23fc8be6590863b8db9ee17b64b7ef0f0a2f9113
jkmin3 <ijoshuamin@gmail.com>
2023-07-26 14:56:55 -0500

    Fixed bug

--- ai_ta_backend/web_scrape.py
+++ ai_ta_backend/web_scrape.py
@@ -41,7 +41,7 @@ def get_urls_list(url:str):
     '''Function gets titles of urls and the urls themselves'''
     # Get rid of double slashes in url
     # Create a base site for incomplete hrefs
-    if url.startswith("https:")
+    if url.startswith("https:"):
       site= re.match(pattern=r'https:\/\/[a-zA-Z0-9.]*[a-z]', string=url).group(0) # type: ignore
       url = re.sub(pattern=r"https:\/\/", repl="", string=url)
       url = re.sub(pattern=r"[\/\/]{2,}", repl="", string=url)
