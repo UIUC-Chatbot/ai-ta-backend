805936bcf1b7cd985dbd2713d4385c55730bacdf
Kastan Day <kastanvday@gmail.com>
2023-09-05 14:01:15 -0700

    increasing timeout from 30 sec to 30 min, web scrape takes a while sometimes

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Docs https://docs.gunicorn.org/en/stable/settings.html#workers
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/ai_ta_backend
-exec gunicorn --workers=2 --threads=16 --worker-class=gthread ai_ta_backend.main:app --timeout 60 --max-requests 2
+exec gunicorn --workers=2 --threads=16 --worker-class=gthread ai_ta_backend.main:app --timeout 1800 --max-requests 2
