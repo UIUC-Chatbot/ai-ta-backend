fcd85daa9a44cb2bc348ec1d54459c9d683c96b4
Kastan Day <kastanvday@gmail.com>
2023-09-05 10:43:14 -0700

    reducing workers, increasing threads. should reduce memory footprint

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Docs https://docs.gunicorn.org/en/stable/settings.html#workers
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/ai_ta_backend
-exec gunicorn --workers=6 --threads=6 --worker-class=gthread ai_ta_backend.main:app --timeout 60 --max-requests 2
+exec gunicorn --workers=3 --threads=10 --worker-class=gthread ai_ta_backend.main:app --timeout 60 --max-requests 2
