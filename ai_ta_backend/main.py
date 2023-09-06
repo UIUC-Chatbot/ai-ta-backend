dc262bbb212e4b63cb22b4181b4e35d6c0f0dc49
star-nox <dabholkar.asmita@gmail.com>
2023-09-06 11:49:00 -0500

    added prints for test

--- ai_ta_backend/main.py
+++ ai_ta_backend/main.py
@@ -342,8 +342,8 @@ def logToNomic():
   print("course_name: ", course_name)
   print("conversation: ", conversation)
 
-  print(request.headers)
-  print(request.get_json())
+  exit()
+
 
   if course_name == '' or conversation == '':
     # proper web error "400 Bad request"
