52f885a6ecdbdbdc2112b5f9d3527192c7769448
Minu Mathew <minumpmathews@gmail.com>
2023-11-21 15:13:21 -0600

    spacing changes

--- ai_ta_backend/agents/ml4bio_agent.py
+++ ai_ta_backend/agents/ml4bio_agent.py
@@ -15,44 +15,50 @@ from .memorycallbacks import get_memory_callback_handler
 
 
 def get_user_info_string():
-  username = getpass.getuser()
-  current_working_directory = os.getcwd()
-  operating_system = platform.system()
-  default_shell = os.environ.get("SHELL")
+    username = getpass.getuser()
+    current_working_directory = os.getcwd()
+    operating_system = platform.system()
+    default_shell = os.environ.get("SHELL")
 
-  return f"[User Info]\nName: {username}\nCWD: {current_working_directory}\nSHELL: {default_shell}\nOS: {operating_system}"
+    return f"[User Info]\nName: {username}\nCWD: {current_working_directory}\nSHELL: {default_shell}\nOS: {operating_system}"
 
 
 class WorkflowAgent:
-  def __init__(self, run_id_in_metadata):
-    if os.environ['OPENAI_API_TYPE'] == 'azure':
-      self.llm = AzureChatOpenAI(temperature=0, model="gpt-4-0613", max_retries=3, request_timeout=60 * 3, deployment_name=os.environ['AZURE_OPENAI_ENGINE'])  # type: ignore
-    else:
-      self.llm: ChatOpenAI = ChatOpenAI(temperature=0, model="gpt-4-0613",max_retries=500, request_timeout=60 * 3)  # type: ignore
-    self.agent = self.make_agent()
-    self.run_id_in_metadata = run_id_in_metadata
+    def __init__(self, run_id_in_metadata, image_name):
+        self.run_id_in_metadata = run_id_in_metadata
+        self.image_name = image_name
+        self.memory_handler = memorycallbacks.MemoryCallbackHandler(run_id=self.run_id_in_metadata,
+                                                                    image_name=self.image_name)
+        if os.environ['OPENAI_API_TYPE'] == 'azure':
+            self.llm = AzureChatOpenAI(temperature=0, model="gpt-4-0613", max_retries=3, request_timeout=60 * 3,
+                                       deployment_name=os.environ['AZURE_OPENAI_ENGINE'],
+                                       callbacks=[self.memory_handler])  # type: ignore
+        else:
+            self.llm: ChatOpenAI = ChatOpenAI(temperature=0, model="gpt-4-0613", max_retries=500,
+                                              request_timeout=60 * 3,
+                                              callbacks=[self.memory_handler])  # type: ignore
 
-  def run(self, input):
-    result = self.agent.with_config({"run_name": "ML4BIO Plan & Execute Agent"}).invoke({"input":f"{input}"}, {"metadata": {"run_id_in_metadata": str(self.run_id_in_metadata)}})
-    
-    print(f"Result: {result}")
-    return result
+        self.agent = self.make_agent()
 
+    def run(self, input):
+        result = self.agent.with_config({"run_name": "ML4BIO Plan & Execute Agent"}).invoke({"input": f"{input}"}, {
+            "metadata": {"run_id_in_metadata": str(self.run_id_in_metadata)}})
 
-  def make_agent(self): 
-    # TOOLS
-    tools = get_tools()
-    memory_handler = get_memory_callback_handler()
+        print(f"Result: {result}")
+        return result
 
-    # PLANNER
-    planner = load_chat_planner(self.llm, system_prompt=hub.pull("kastanday/ml4bio-rnaseq-planner").format(user_info=get_user_info_string))
+    def make_agent(self):
+        # TOOLS
+        tools = get_tools()
 
+        # PLANNER
+        planner = load_chat_planner(self.llm, system_prompt=hub.pull("kastanday/ml4bio-rnaseq-planner").format(user_info = get_user_info_string))
 
-    # EXECUTOR
-    # executor = load_agent_executor(self.llm, tools, verbose=True, trim_intermediate_steps=fancier_trim_intermediate_steps, handle_parsing_errors=True)
-    executor = load_agent_executor(self.llm, tools, verbose=True, handle_parsing_errors=True)
+        # EXECUTOR
+        # executor = load_agent_executor(self.llm, tools, verbose=True, trim_intermediate_steps=fancier_trim_intermediate_steps, handle_parsing_errors=True)
+        executor = load_agent_executor(self.llm, tools, verbose=True, handle_parsing_errors=True)
 
-    # Create PlanAndExecute Agent
-    workflow_agent = PlanAndExecute(planner=planner, executor=executor, verbose=True, callbacks=[memory_handler])
+        # Create PlanAndExecute Agent
+        workflow_agent = PlanAndExecute(planner=planner, executor=executor, verbose=True, callbacks=[self.memory_handler])
 
-    return workflow_agent
+        return workflow_agent
