84f807111f9eb8b73df966e7018f1ab4529c95d1
Minu Mathew <minumpmathews@gmail.com>
2023-11-27 15:58:45 -0600

    custom callbacks with db updates

--- ai_ta_backend/agents/customcallbacks.py
+++ ai_ta_backend/agents/customcallbacks.py
@@ -1,4 +1,5 @@
 from typing import Dict, Union, Any, List
+import re
 
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish
@@ -6,15 +7,12 @@ from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import tracing_enabled
 from langchain.llms import OpenAI
 from langchain import hub
-from ai_ta_backend.agents.utils import get_supabase_client, get_langsmith_id
+from ai_ta_backend.agents.utils import SupabaseDB, get_langsmith_id
 
 
 class CustomCallbackHandler(BaseCallbackHandler):
     """A callback handler that stores the LLM's context and action in memory."""
     def __init__(self, run_id=None, image_name=None):
-        self.tool_in_progress = False  # usage TBD
-
-        self.supabase_client = get_supabase_client()
         if run_id:
             self.langsmith_run_id = run_id
         else:
@@ -23,37 +21,51 @@ class CustomCallbackHandler(BaseCallbackHandler):
         if image_name:
             self.image_name = image_name
         else:
-            self.image_name = None
+            self.image_name = ''
 
-    def is_exists_image(self) -> bool:
-        langsmith_response = self.supabase_client.table("docker_images").select("image_name").\
-                                        eq("image_name", self.image_name).execute()
-        return len(langsmith_response.data) > 0
+        self.tool_in_progress = {"status": False, "name": ""}
+        self.db = SupabaseDB(table_name="docker_images", image_name=self.image_name)
+
 
     def on_tool_start(self, serialized: Dict[str, Any], input_str: str, **kwargs: Any) -> Any:
-        print(f"on_tool_start {serialized}")
-        if self.is_exists_image():
-            response = self.supabase_client.table("docker_images").update({"on_tool_start": str(serialized)}).\
-                                        eq("image_name", self.image_name).execute()
+        """Callback for when a tool starts.
+        Set tool_in_progress to True and store the tool's name in memory."""
+
+        self.tool_in_progress['status'] = True
+        self.tool_in_progress['name'] = serialized['name']
+
+        if self.db.is_exists_image():
+            data = self.db.fetch_field_from_db("on_tool_start")
+            if data:
+                data.append(serialized)
+                response = self.db.update_field_in_db("on_tool_start", data)
+            else:
+                response = self.db.update_field_in_db("on_tool_start", serialized)
         else:
-            response = self.supabase_client.table("docker_images").upsert(
-                {"langsmith_id": self.langsmith_run_id, "image_name": self.image_name, "on_tool_start": str(serialized)}).execute()
-        self.tool_in_progress = True
-        print(response.data)
+            response = self.db.upsert_field_in_db("on_tool_start", serialized)
+
 
     def on_tool_end(self, output: str, **kwargs: Any) -> Any:
-        """Run when LLM errors."""
-        print(f"On tool end: {output}")
-        if self.is_exists_image():
-            response = self.supabase_client.table("docker_images").update({"on_tool_end": str(output)}).\
-                                        eq("image_name", self.image_name).execute()
-        else:
-            response = self.supabase_client.table("docker_images").upsert(
-                {"langsmith_id": self.langsmith_run_id, "image_name": self.image_name, "on_tool_end": str(output)}).execute()
+        """Callback for when a tool ends.
+        Use this to store the tool's output in a database. Use tool start parameters to identify the tool."""
 
-        if self.tool_in_progress:
-            self.tool_in_progress = False
-            print(f"Tool output: {output}")
+        if self.tool_in_progress['status']:
+            tool_name = self.tool_in_progress['name']
+            output = {"name": tool_name, "output": output}
+            # reset tool_in_progress
+            self.tool_in_progress['status'] = False
+            self.tool_in_progress['name'] = ''
+
+        if self.db.is_exists_image():
+            data = self.db.fetch_field_from_db("on_tool_end")
+            if data:
+                data.append(output)
+                response = self.db.update_field_in_db("on_tool_end", data)
+            else:
+                response = self.db.update_field_in_db("on_tool_end", output)
+        else:
+            response = self.db.upsert_field_in_db("on_tool_end", output)
+
 
     def on_tool_error(self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any) -> Any:
         """Run when LLM errors."""
@@ -64,22 +76,37 @@ class CustomCallbackHandler(BaseCallbackHandler):
         pass
 
     def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
-        print(f"on_agent_action {action}")
-        if self.is_exists_image():
-            self.supabase_client.table("docker_images").update({"on_agent_action": str(action)}).\
-                                        eq("image_name", self.image_name).execute()
+        """Run when LLM predicts an action. Parse the action and store it in a database."""
+        action = action.dict()
+        # Use regex to delete everything after two consecutive newlines
+        action['log'] = re.sub(r'\n\n.*', '', action['log'], flags=re.DOTALL)
+        #action['log'] = action['log'].replace(["\n", "```"], "")
+
+        if self.db.is_exists_image():
+            data = self.db.fetch_field_from_db("on_agent_action")
+            if data:
+                data.append(action)
+                response = self.db.update_field_in_db("on_agent_action", data)
+            else:
+                response = self.db.update_field_in_db("on_agent_action", action)
         else:
-            self.supabase_client.table("docker_images").upsert(
-                {"langsmith_id": self.langsmith_run_id, "image_name": self.image_name, "on_agent_action": str(action)}).execute()
+            response = self.db.upsert_field_in_db("on_agent_action", action)
+
 
     def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> Any:
-        print(f"on_agent_finish {finish}")
-        if self.is_exists_image():
-            self.supabase_client.table("docker_images").update({"on_agent_finish": str(finish)}).\
-                                        eq("image_name", self.image_name).execute()
+        """Run when LLM finishes. Store the finish in a database."""
+        finish = finish.dict()
+
+        if self.db.is_exists_image():
+            data = self.db.fetch_field_from_db("on_agent_finish")
+            if data:
+                data.append(finish)
+                response = self.db.update_field_in_db("on_agent_finish", data)
+            else:
+                response = self.db.update_field_in_db("on_agent_finish", finish)
         else:
-            self.supabase_client.table("docker_images").upsert(
-                {"langsmith_id": self.langsmith_run_id, "image_name": self.image_name, "on_agent_finish": str(finish)}).execute()
+            response = self.db.upsert_field_in_db("on_agent_finish", finish)
+
 
     # def on_llm_start(self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any) -> Any:
     #     print(f"on_llm_start {serialized}")
@@ -87,14 +114,9 @@ class CustomCallbackHandler(BaseCallbackHandler):
     # def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
     #     print(f"on_new_token {token}")
 
-    # def on_chain_start(self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any) -> Any:
-    #     print("on_chain_start")
-    #     if self.is_exists_image():
-    #         self.supabase_client.table("docker_images").update({"on_chain_start": str(serialized)}).\
-    #                                     eq("image_name", self.image_name).execute()
-    #     else:
-    #         self.supabase_client.table("docker_images").upsert(
-    #             {"langsmith_id": self.langsmith_run_id, "image_name": self.image_name, "on_chain_start": str(serialized)}).execute()
+    def on_chain_start(self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any) -> Any:
+        """Callback for when a chain starts."""
+        pass
 
 
 def get_custom_callback_handler():
