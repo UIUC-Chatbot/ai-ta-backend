5a41047b9128c2ebacbf8f9ae20013f2add027c8
Kastan Day <kastanvday@gmail.com>
2023-12-14 11:13:49 -0800

    Adding pydantic v1 req, and ray[default]

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 # Docs https://docs.gunicorn.org/en/stable/settings.html#workers
 
+pip list
+
 ray start --head
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/ai_ta_backend
 exec gunicorn --workers=3 --threads=16 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
