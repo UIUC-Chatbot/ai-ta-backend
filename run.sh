9bbfb1517fc2fa5e81ad1e43b9fdace7cbcbe481
Kastan Day <kastanvday@gmail.com>
2023-12-18 14:35:14 -0800

    Fix ray start --head to have memory available for Railway

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/ai_ta_backend
-ray start --head
+ray start --head --num-cpus 6 --object-store-memory 400000000
 exec newrelic-admin run-program gunicorn --workers=6 --threads=6 --worker-class=gthread ai_ta_backend.main:app --timeout 108000
