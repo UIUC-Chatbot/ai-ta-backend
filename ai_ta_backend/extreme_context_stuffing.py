49700b33edcbd1b6181fcaf813b4a965e0fea997
Kastan Day <kastanvday@gmail.com>
2023-12-18 08:47:35 -0800

    Add support for Azure embedding model to our parallel processor

--- ai_ta_backend/extreme_context_stuffing.py
+++ ai_ta_backend/extreme_context_stuffing.py
@@ -366,8 +366,11 @@ class APIRequest:
 
 def api_endpoint_from_url(request_url: str):
   """Extract the API endpoint from the request URL."""
-  match = re.search('^https://[^/]+/v\\d+/(.+)$', request_url)
-  return match[1]  # type: ignore
+  if 'text-embedding-ada-002' in request_url:
+    return 'embeddings'
+  else:
+    match = re.search('^https://[^/]+/v\\d+/(.+)$', request_url)
+    return match[1]  # type: ignore
 
 
 def append_to_jsonl(data, filename: str) -> None:
