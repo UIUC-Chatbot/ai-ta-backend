88aefddd48af7a6238567d3e10ab2ef11cddab7a
Minu Mathew <minumpmathews@gmail.com>
2024-02-01 11:59:49 -0600

    agentState not keeping track of intermediate steps

--- ai_ta_backend/agents/langgraph_agent.py
+++ ai_ta_backend/agents/langgraph_agent.py
@@ -41,11 +41,8 @@ class AgentState(TypedDict):
   # The outcome of a given call to the agent
   # Needs `None` as a valid type, since this is what this will start as
   agent_outcome: Union[AgentAction, AgentFinish, None]
-  # List of actions and corresponding observations
-  # Here we annotate this with `operator.add` to indicate that operations to
-  # this state should be ADDED to the existing values (not overwrite it)
-  intermediate_steps: Annotated[list[tuple[AgentAction, str]], operator.add]
-
+  # intermediate steps are present in agent input arg as well
+  #intermediate_steps: Annotated[list[tuple[AgentAction, str]], operator.add]
   plan: Union[list[str], None]
 
 
