e44da040b3894f85569a66f2c300035a53e523a1
star-nox <dabholkar.asmita@gmail.com>
2024-02-05 17:06:20 -0600

    added function for document export

--- ai_ta_backend/export_data.py
+++ ai_ta_backend/export_data.py
@@ -5,6 +5,11 @@ import pandas as pd
 import supabase
 import sentry_sdk
 
+import zipfile
+
+SUPABASE_CLIENT = supabase.create_client(  # type: ignore
+      supabase_url=os.getenv('SUPABASE_URL'),  # type: ignore
+      supabase_key=os.getenv('SUPABASE_API_KEY'))  # type: ignore
 
 def export_documents_csv(course_name: str, from_date='', to_date=''):
   """
@@ -14,8 +19,72 @@ def export_documents_csv(course_name: str, from_date='', to_date=''):
       from_date (str, optional): The start date for the data export. Defaults to ''.
       to_date (str, optional): The end date for the data export. Defaults to ''.
   """
+  print("Exporting documents to csv file...")
 
-  return "success"
+  if from_date != '' and to_date != '':
+    # query between the dates
+    print("from_date and to_date")
+    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).gte('created_at', from_date).lte('created_at', to_date).order('id', desc=False).execute()
+  
+  elif from_date != '' and to_date == '':
+    # query from from_date to now
+    print("only from_date")
+    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).gte('created_at', from_date).order('id', desc=False).execute()
+
+  elif from_date == '' and to_date != '':
+    # query from beginning to to_date
+    print("only to_date")
+    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).lte('created_at', to_date).order('id', desc=False).execute()
+
+  else:
+    # query all data
+    print("No dates")
+    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).order('id', desc=False).execute()
+
+  # Fetch data
+  if response.count > 0:
+    # batch download
+    total_doc_count = response.count
+    first_id = response.data[0]['id']
+    last_id = response.data[-1]['id']
+    curr_doc_count = 0
+    filename = course_name + '_' + str(uuid.uuid4()) + '_documents.csv'
+    file_path = os.path.join(os.getcwd(), filename)
+
+    while curr_doc_count < total_doc_count:
+      print("Fetching data from id: ", first_id)
+      response = SUPABASE_CLIENT.table("documents").select("*").eq("course_name", course_name).gte('id', first_id).lte('id', last_id).order('id', desc=False).limit(25).execute()
+      df = pd.DataFrame(response.data)
+      
+      if not os.path.isfile(file_path):
+        df.to_csv(file_path, mode='a', header=True, index=False)
+      else:
+        df.to_csv(file_path, mode='a', header=False, index=False)
+        
+      first_id = response.data[-1]['id'] + 1
+      curr_doc_count += len(response.data)
+
+    # Download file
+    try:
+      # zip file
+      zip_filename = filename.split('.')[0] + '.zip'
+      zip_file_path = os.path.join(os.getcwd(), zip_filename)
+
+      with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(file_path, filename)
+      
+      os.remove(file_path)
+      
+      return (zip_file_path, zip_filename, os.getcwd())
+    except Exception as e:
+      print(e)
+      sentry_sdk.capture_exception(e)
+      return "Error downloading file"
+
+  else:
+    return "No data found between the dates"
+
+
 
 def export_convo_history_csv(course_name: str, from_date='', to_date=''):
   """
@@ -26,29 +95,27 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
       to_date (str, optional): The end date for the data export. Defaults to ''.
   """
   print("Exporting conversation history to csv file...")
-  supabase_client = supabase.create_client(  # type: ignore
-      supabase_url=os.getenv('SUPABASE_URL'),  # type: ignore
-      supabase_key=os.getenv('SUPABASE_API_KEY'))  # type: ignore
+
 
   if from_date == '' and to_date == '':
     # Get all data
     print("No dates")
-    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).order('id', desc=False).execute()
   elif from_date != '' and to_date == '':
     print("only from_date")
     # Get data from from_date to now
-    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).gte('created_at', from_date).order('id', desc=False).execute()
   elif from_date == '' and to_date != '':
     print("only to_date")
     # Get data from beginning to to_date
-    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).lte('created_at', to_date).order('id', desc=False).execute()
   else:
     print("both from_date and to_date")
     # Get data from from_date to to_date
-    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).gte('created_at', from_date).lte('created_at', to_date).order('id',
                                                                                                   desc=False).execute()
 
@@ -63,7 +130,7 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
     # Fetch data in batches of 25 from first_id to last_id
     while first_id <= last_id:
       print("Fetching data from id: ", first_id)
-      response = supabase_client.table("llm-convo-monitor").select("*").eq("course_name", course_name).gte(
+      response = SUPABASE_CLIENT.table("llm-convo-monitor").select("*").eq("course_name", course_name).gte(
           'id', first_id).lte('id', last_id).order('id', desc=False).limit(25).execute()
       # Convert to pandas dataframe
       df = pd.DataFrame(response.data)
@@ -79,7 +146,15 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
 
     # Download file
     try:
-      return (file_path, filename, os.getcwd())
+      # zip file
+      zip_filename = filename.split('.')[0] + '.zip'
+      zip_file_path = os.path.join(os.getcwd(), zip_filename)
+
+      with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(file_path, filename)
+      os.remove(file_path)
+      
+      return (zip_file_path, zip_filename, os.getcwd())
     except Exception as e:
       print(e)
       sentry_sdk.capture_exception(e)
