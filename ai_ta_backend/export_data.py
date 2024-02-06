968d374b4c25c06aac9fbc42f9cd83b95a5f3054
Kastan Day <kastanvday@gmail.com>
2024-02-06 15:19:10 -0800

    Move Supabase inside functions

--- ai_ta_backend/export_data.py
+++ ai_ta_backend/export_data.py
@@ -1,15 +1,11 @@
 import os
 import uuid
+import zipfile
 
 import pandas as pd
 import supabase
 import sentry_sdk
 
-import zipfile
-
-SUPABASE_CLIENT = supabase.create_client(  # type: ignore
-      supabase_url=os.getenv('SUPABASE_URL'),  # type: ignore
-      supabase_key=os.getenv('SUPABASE_API_KEY'))  # type: ignore
 
 def export_documents_csv(course_name: str, from_date='', to_date=''):
   """
@@ -21,25 +17,35 @@ def export_documents_csv(course_name: str, from_date='', to_date=''):
   """
   print("Exporting documents to csv file...")
 
+  supabase_client = supabase.create_client(  # type: ignore
+      supabase_url=os.getenv('SUPABASE_URL'),  # type: ignore
+      supabase_key=os.getenv('SUPABASE_API_KEY'))  # type: ignore
+
   if from_date != '' and to_date != '':
     # query between the dates
     print("from_date and to_date")
-    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).gte('created_at', from_date).lte('created_at', to_date).order('id', desc=False).execute()
-  
+    response = supabase_client.table("documents").select("id", count='exact').eq("course_name", course_name).gte(
+        'created_at', from_date).lte('created_at', to_date).order('id', desc=False).execute()
+
   elif from_date != '' and to_date == '':
     # query from from_date to now
     print("only from_date")
-    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).gte('created_at', from_date).order('id', desc=False).execute()
+    response = supabase_client.table("documents").select("id", count='exact').eq("course_name", course_name).gte(
+        'created_at', from_date).order('id', desc=False).execute()
 
   elif from_date == '' and to_date != '':
     # query from beginning to to_date
     print("only to_date")
-    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).lte('created_at', to_date).order('id', desc=False).execute()
+    response = supabase_client.table("documents").select("id", count='exact').eq("course_name", course_name).lte(
+        'created_at', to_date).order('id', desc=False).execute()
 
   else:
     # query all data
     print("No dates")
-    response = SUPABASE_CLIENT.table("documents").select("id", count='exact').eq("course_name", course_name).order('id', desc=False).execute()
+    response = supabase_client.table("documents").select("id",
+                                                         count='exact').eq("course_name",
+                                                                           course_name).order('id',
+                                                                                              desc=False).execute()
 
   # Fetch data
   if response.count > 0:
@@ -53,14 +59,15 @@ def export_documents_csv(course_name: str, from_date='', to_date=''):
 
     while curr_doc_count < total_doc_count:
       print("Fetching data from id: ", first_id)
-      response = SUPABASE_CLIENT.table("documents").select("*").eq("course_name", course_name).gte('id', first_id).lte('id', last_id).order('id', desc=False).limit(25).execute()
+      response = supabase_client.table("documents").select("*").eq("course_name", course_name).gte('id', first_id).lte(
+          'id', last_id).order('id', desc=False).limit(25).execute()
       df = pd.DataFrame(response.data)
-      
+
       if not os.path.isfile(file_path):
         df.to_csv(file_path, mode='a', header=True, index=False)
       else:
         df.to_csv(file_path, mode='a', header=False, index=False)
-        
+
       first_id = response.data[-1]['id'] + 1
       curr_doc_count += len(response.data)
 
@@ -72,9 +79,9 @@ def export_documents_csv(course_name: str, from_date='', to_date=''):
 
       with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
         zipf.write(file_path, filename)
-      
+
       os.remove(file_path)
-      
+
       return (zip_file_path, zip_filename, os.getcwd())
     except Exception as e:
       print(e)
@@ -83,7 +90,6 @@ def export_documents_csv(course_name: str, from_date='', to_date=''):
 
   else:
     return "No data found between the dates"
-
 
 
 def export_convo_history_csv(course_name: str, from_date='', to_date=''):
@@ -96,26 +102,29 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
   """
   print("Exporting conversation history to csv file...")
 
+  supabase_client = supabase.create_client(  # type: ignore
+      supabase_url=os.getenv('SUPABASE_URL'),  # type: ignore
+      supabase_key=os.getenv('SUPABASE_API_KEY'))  # type: ignore
 
   if from_date == '' and to_date == '':
     # Get all data
     print("No dates")
-    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).order('id', desc=False).execute()
   elif from_date != '' and to_date == '':
     print("only from_date")
     # Get data from from_date to now
-    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).gte('created_at', from_date).order('id', desc=False).execute()
   elif from_date == '' and to_date != '':
     print("only to_date")
     # Get data from beginning to to_date
-    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).lte('created_at', to_date).order('id', desc=False).execute()
   else:
     print("both from_date and to_date")
     # Get data from from_date to to_date
-    response = SUPABASE_CLIENT.table("llm-convo-monitor").select("id", count='exact').eq(
+    response = supabase_client.table("llm-convo-monitor").select("id", count='exact').eq(
         "course_name", course_name).gte('created_at', from_date).lte('created_at', to_date).order('id',
                                                                                                   desc=False).execute()
 
@@ -130,7 +139,7 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
     # Fetch data in batches of 25 from first_id to last_id
     while first_id <= last_id:
       print("Fetching data from id: ", first_id)
-      response = SUPABASE_CLIENT.table("llm-convo-monitor").select("*").eq("course_name", course_name).gte(
+      response = supabase_client.table("llm-convo-monitor").select("*").eq("course_name", course_name).gte(
           'id', first_id).lte('id', last_id).order('id', desc=False).limit(25).execute()
       # Convert to pandas dataframe
       df = pd.DataFrame(response.data)
@@ -153,7 +162,7 @@ def export_convo_history_csv(course_name: str, from_date='', to_date=''):
       with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
         zipf.write(file_path, filename)
       os.remove(file_path)
-      
+
       return (zip_file_path, zip_filename, os.getcwd())
     except Exception as e:
       print(e)
