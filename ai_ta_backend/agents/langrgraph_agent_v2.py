04a5a4309787c6336106c608f950d169c234b5e4
rohanmarwaha <rohan13@illinois.edu>
2024-02-22 00:26:50 +0530

    Integrating demo plan and execute notebook with existing agent

---
+++ ai_ta_backend/agents/langrgraph_agent_v2.py
@@ -0,0 +1,146 @@
+import getpass
+import os
+import platform
+from typing import List, Tuple, Union, Annotated, TypedDict
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
+from langchain.chains.openai_functions import create_structured_output_runnable, create_openai_fn_runnable
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import create_agent_executor
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.messages import HumanMessage, BaseMessage
+import operator
+
+from ai_ta_backend.agents.tools import get_tools
+
+load_dotenv(override=True)
+
+
+class Plan(BaseModel):
+  """Plan to follow in future"""
+  steps: List[str] = Field(description="different steps to follow, should be in sorted order")
+
+
+class Response(BaseModel):
+  """Response to user."""
+  response: str
+
+
+class PlanExecute(TypedDict):
+  input: str
+  chat_history: list[BaseMessage]
+  plan: List[str]
+  past_steps: Annotated[List[Tuple], operator.add]
+  response: str
+
+
+def get_user_info_string():
+  username = getpass.getuser()
+  current_working_directory = os.getcwd()
+  operating_system = platform.system()
+  default_shell = os.environ.get("SHELL")
+
+  return f"[User Info]\nName: {username}\nCWD: {current_working_directory}\nSHELL: {default_shell}\nOS: {operating_system}"
+
+
+def get_llm():
+  if os.getenv('OPENAI_API_TYPE') == 'azure':
+    return AzureChatOpenAI(
+        azure_deployment="gpt-4-128k",
+        openai_api_version=os.getenv("AZURE_0125_MODEL_VERSION"),
+        temperature=0,
+        azure_endpoint=os.getenv("AZURE_0125_MODEL_ENDPOINT"),
+        openai_api_key=os.getenv("AZURE_0125_MODEL_API_KEY"),
+    )
+  else:
+    return ChatOpenAI(
+        model="gpt-4-turbo-preview",
+        temperature=0,
+    )
+
+
+class WorkflowAgent:
+
+  def __init__(self, langsmith_run_id):
+    self.langsmith_run_id = langsmith_run_id
+    self.llm = get_llm()
+    self.tools = get_tools(langsmith_run_id)
+    self.planner_prompt = ChatPromptTemplate.from_template(
+        """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+{objective}""")
+    self.replanner_prompt = ChatPromptTemplate.from_template(
+        """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+Your objective was this:
+{input}
+
+Your original plan was this:
+{plan}
+
+You have currently done the follow steps:
+{past_steps}
+
+Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that. Otherwise, fill out the plan. Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan."""
+    )
+
+    self.agent_runnable = create_openai_functions_agent(self.llm, self.tools,
+                                                        hub.pull("hwchase17/openai-functions-agent"))
+    self.agent_executor = create_agent_executor(self.agent_runnable, self.tools)
+    self.workflow = self.create_workflow()
+
+  def create_workflow(self):
+    workflow = StateGraph(PlanExecute)
+
+    async def execute_step(state: PlanExecute):
+      task = state["plan"][0]
+      agent_response = await self.agent_executor.ainvoke({"input": task, "chat_history": []})
+      return {"past_steps": (task, agent_response["agent_outcome"].return_values["output"])}
+
+    async def plan_step(state: PlanExecute):
+      planner = create_structured_output_runnable(Plan, self.llm, self.planner_prompt)
+      plan = await planner.ainvoke({"objective": state["input"]})
+      return {"plan": plan.steps}
+
+    async def replan_step(state: PlanExecute):
+      replanner = create_openai_fn_runnable([Plan, Response], self.llm, self.replanner_prompt)
+      output = await replanner.ainvoke(state)
+      if isinstance(output, Response):
+        return {"response": output.response}
+      else:
+        return {"plan": output.steps}
+
+    def should_end(state: PlanExecute):
+      if state["response"]:
+        return True
+      else:
+        return False
+
+    workflow.add_node("planner", plan_step)
+    workflow.add_node("agent", execute_step)
+    workflow.add_node("replan", replan_step)
+    workflow.set_entry_point("planner")
+    workflow.add_edge("planner", "agent")
+    workflow.add_edge("agent", "replan")
+    workflow.add_conditional_edges("replan", should_end, {True: END, False: "agent"})  #type: ignore
+
+    return workflow.compile()
+
+  async def run(self, input_prompt):
+    inputs = {"input": input_prompt}
+    async for event in self.workflow.astream(inputs, config={"recursion_limit": 50}):
+      for k, v in event.items():
+        if k != "__end__":
+          print(v)
+
+
+# Example usage
+# agent = WorkflowAgent()
+# await agent.run("what is
