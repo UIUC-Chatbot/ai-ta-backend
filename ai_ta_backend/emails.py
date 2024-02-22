ef4c016cfe6fcc0b5a1b6dc6c5dc3ab5ebbf44c6
Asmita Dabholkar <dabholkar.asmita@gmail.com>
2024-02-22 12:05:36 -0600

    BCC recipients in Document export email (#224)
    * added BCC receipients in send_email()
    
    * added export endpoint again, it dissapeared during merge

--- ai_ta_backend/emails.py
+++ ai_ta_backend/emails.py
@@ -4,16 +4,25 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def send_email(subject, body_text, sender, receipients):
+def send_email(subject: str, body_text: str, sender: str, receipients: list, bcc_receipients: list):
+    """
+    Send an email using the AWS SES service
+    :param subject: The subject of the email
+    :param body_text: The body of the email
+    :param sender: The email address of the sender
+    :param receipients: A list of email addresses to send the email to
+    :param bcc_receipients: A list of email addresses to send the email to as BCC
+    :return: A string indicating the result of the email send operation
+
+    """
     # Create message content
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
     message["From"] = sender
+    message["To"] = ", ".join(receipients)
 
-    if len(receipients) == 1:
-        message["To"] = receipients[0]
-    else:
-        message["To"] = ", ".join(receipients)
+    if len(bcc_receipients) > 0:
+        message["Bcc"] = ", ".join(bcc_receipients)
 
     # Add plain text part
     part1 = MIMEText(body_text, "plain")
@@ -24,6 +33,6 @@ def send_email(subject, body_text, sender, receipients):
     # Connect to SMTP server
     with smtplib.SMTP_SSL(os.getenv('SES_HOST'), os.getenv('SES_PORT')) as server: # type: ignore
         server.login(os.getenv('USERNAME_SMTP'), os.getenv('PASSWORD_SMTP'))    # type: ignore
-        server.sendmail(sender, receipients, message.as_string())
+        server.sendmail(sender, receipients + bcc_receipients, message.as_string())
 
     return "Email sent successfully!"
