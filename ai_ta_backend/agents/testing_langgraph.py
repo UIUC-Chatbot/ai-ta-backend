d07d2ef3979c98b4611f34a7ed1f8afc288e8ce9
rohanmarwaha <rohan13@illinois.edu>
2024-02-23 00:05:01 +0530

    Updated test file for agent v2

--- ai_ta_backend/agents/testing_langgraph.py
+++ ai_ta_backend/agents/testing_langgraph.py
@@ -6,7 +6,7 @@ python -m ai_ta_backend.agents.testing_langgraph
 import uuid
 
 import langchain
-from ai_ta_backend.agents.langgraph_agent import AgentState, WorkflowAgent
+from ai_ta_backend.agents.langrgraph_agent_v2 import PlanExecute, WorkflowAgent
 from langchain import hub
 from dotenv import load_dotenv
 
@@ -15,17 +15,23 @@ load_dotenv(override=True)
 # langchain.debug = True  # True for more detailed logs
 # VERBOSE = True
 
+import asyncio
+
 if __name__ == '__main__':
-  id = uuid.uuid4()
-  a = WorkflowAgent(id)
-  a.run("Write a function to calculate the mean of a list of numbers.")
+
+  async def main():
+    id = uuid.uuid4()
+    a = WorkflowAgent(id)
+    await a.run("Write a function to calculate the mean of a list of numbers.")
+
+  asyncio.run(main())
 
 # print("-------- OPENAI_API_BASE", os.environ['OPENAI_API_BASE'])
 # print("-------- OPENAI_API_TYPE", os.environ['OPENAI_API_TYPE'])
 # print("-------- AZURE_ENDPOINT", os.environ['AZURE_ENDPOINT'])
 
 
-def stateToPrompt(state: AgentState, token_limit: int = 8_000):
+def stateToPrompt(state: PlanExecute, token_limit: int = 8_000):
   """
   Memory prompt: https://smith.langchain.com/hub/kastanday/memory_manager_agent
   Inputs = ['github_issue', 'messages_with_human', 'plan', 'tool_use_history']
@@ -39,13 +45,7 @@ def stateToPrompt(state: AgentState, token_limit: int = 8_000):
       # user_info=get_user_info_string(),
       input=state['input'],
       chat_history='\n'.join([f"User: {message.content}" for message in state['chat_history']]),
-      agent_outcome=state['agent_outcome'],
-      intermediate_steps='\n'.join([f"{action}: {observation}" for action, observation in state['intermediate_steps']
+      agent_outcome=state['response'],
+      intermediate_steps='\n'.join([f"{action}: {observation}" for action, observation in state['past_steps']
                                    ]),  # type: ignore
   )
-
-
-if __name__ == '__main__':
-  id = uuid.uuid4()
-  a = WorkflowAgent(id)
-  a.run("Write a function to calculate the mean of a list of numbers.")
