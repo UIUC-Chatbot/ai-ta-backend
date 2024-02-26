2e5a5739055f718292eb6bef0ad8eb0b7af0f9b3
star-nox <dabholkar.asmita@gmail.com>
2024-02-26 12:54:07 -0600

    minor changes in metadata extraction notebook

--- ai_ta_backend/crawlee_ext_scrape.py
+++ ai_ta_backend/crawlee_ext_scrape.py
@@ -9,9 +9,9 @@ def crawlee_scrape(course_name: str):
     This function takes in a pre-defined set of URLs and scrapes the content from each URL.
     """
     urls = [
-        'https://www.uaex.uada.edu/',
-        'https://ucanr.edu/Focus_Areas/'
-
+        'https://farmdocdaily.illinois.edu',
+        'https://farmpolicynews.illinois.edu',
+        'https://extension.illinois.edu'
     ]
 
     payload = {
@@ -19,7 +19,7 @@ def crawlee_scrape(course_name: str):
                 "url": "",
                 "scrapeStrategy": "equal-and-below",
                 "match": "",
-                "maxPagesToCrawl": 5000,
+                "maxPagesToCrawl": 20000,
                 "maxTokens": 2000000,
                 "courseName": course_name
             }
@@ -28,38 +28,88 @@ def crawlee_scrape(course_name: str):
     # create a POST request to the crawlee API
     api_endpoint = 'https://crawlee-production.up.railway.app/crawl'
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for url in urls:
-            future = executor.submit(scrape_url, url, payload, api_endpoint)
-            futures.append(future)
-
-        # Wait for all tasks to complete and gather results
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            print(result)
-
+    # loop through the URLs and scrape the content
+    for url in urls:
+        payload["params"]["url"] = url
+        payload["params"]["match"] = "http?(s)://" + url.split("//")[1] + "/**"
+        
+        
+        print("Scraping URL:", url)
+        start_time = time.monotonic()
+        response = requests.post(api_endpoint, json=payload)
+        
+        try:
+            no_of_urls_scraped = response.json()
+            SCRAPE_LOG[url] = no_of_urls_scraped
+        except Exception as e:
+            print(f"Error: {e}")
+            
+        print(f"⏰ Scraping runtime: {(time.monotonic() - start_time):.2f} seconds")
+        time.sleep(10)
+    
     print(SCRAPE_LOG)
 
     return "Scraping complete."
 
+# parallel version of the above function
 
-def scrape_url(url, payload, api_endpoint):
-    """
-    Scrapes a single URL and logs results.
-    """
+# def crawlee_scrape(course_name: str):
+#     """
+#     This function takes in a pre-defined set of URLs and scrapes the content from each URL.
+#     """
+#     urls = [
+#         'https://extension.uga.edu'
+        
+#     ]
 
-    payload["params"]["url"] = url
-    payload["params"]["match"] = "http?(s)://" + url.split("//")[1] + "/**"
+#     payload = {
+#             "params": {
+#                 "url": "",
+#                 "scrapeStrategy": "equal-and-below",
+#                 "match": "",
+#                 "maxPagesToCrawl": 5000,
+#                 "maxTokens": 2000000,
+#                 "courseName": course_name
+#             }
+#     }
 
-    print("Scraping URL:", url)
-    start_time = time.monotonic()
-    response = requests.post(api_endpoint, json=payload)
+#     # create a POST request to the crawlee API
+#     api_endpoint = 'https://crawlee-production.up.railway.app/crawl'
 
-    no_of_urls_scraped = response.json()
-    SCRAPE_LOG[url] = no_of_urls_scraped
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         futures = []
+#         for url in urls:
+#             future = executor.submit(scrape_url, url, payload, api_endpoint)
+#             futures.append(future)
 
-    print(f"⏰ Scraping runtime: {(time.monotonic() - start_time):.2f} seconds")
+#         # Wait for all tasks to complete and gather results
+#         for future in concurrent.futures.as_completed(futures):
+#             result = future.result()
+#             print(result)
 
-    return f"Scraped {url} with {no_of_urls_scraped} URLs scraped."
+#     print(SCRAPE_LOG)
+
+#     return "Scraping complete."
+
+
+# def scrape_url(url, payload, api_endpoint):
+#     """
+#     Scrapes a single URL and logs results.
+#     """
+
+#     payload["params"]["url"] = url
+#     payload["params"]["match"] = "http?(s)://" + url.split("//")[1] + "/**"
+
+#     print("Scraping URL:", url)
+#     start_time = time.monotonic()
+#     response = requests.post(api_endpoint, json=payload)
+
+#     #no_of_urls_scraped = response.json()
+#     #SCRAPE_LOG[url] = no_of_urls_scraped
+
+#     no_of_urls_scraped = response.text
+
+#     print(f"⏰ Scraping runtime: {(time.monotonic() - start_time):.2f} seconds")
+
+#     return f"Scraped {url} with {no_of_urls_scraped} URLs scraped."
 
