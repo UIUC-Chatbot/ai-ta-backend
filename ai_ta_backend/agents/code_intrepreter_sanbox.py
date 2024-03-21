2311c70f735b1feb09a920ddd2c5d2368b157d37
Minu Mathew <minumpmathews@gmail.com>
2024-03-21 14:57:30 -0500

    added r code run

--- ai_ta_backend/agents/code_intrepreter_sanbox.py
+++ ai_ta_backend/agents/code_intrepreter_sanbox.py
@@ -41,7 +41,7 @@ class E2B_class():
     self.existing_files = []
     self.working_dir = '/home/user/'
     self.curr_terminal_output = ''
-    # self.install_base_packages()
+    self.install_base_packages()
 
   def __del__(self):
     try:
@@ -50,7 +50,8 @@ class E2B_class():
       print("Failed to close e2b sandbox, probably fine.")
 
   def install_base_packages(self):
-    self.run_shell("pip install -U numpy pandas matplotlib seaborn scikit-learn scipy")
+    #self.run_shell("pip install -U numpy pandas matplotlib seaborn scikit-learn scipy")
+    self.run_shell("sudo apt-get install r-base r-base-dev -y")
 
   def run_python_code(self, code: str):
     print(termcolor.colored("RUNNING PYTHON CODE:", 'blue', attrs=['bold', 'underline']))
@@ -60,8 +61,18 @@ class E2B_class():
     fileid = str(uuid.uuid4())
     code_file.name = fileid + ".py"
     filepath = self.sandbox.upload_file(code_file)
-
     shell_output = self.run_shell(f"python {filepath}")
+    return shell_output
+
+  def run_r_code(self, code: str):
+    print(termcolor.colored("RUNNING R CODE:", 'green', attrs=['bold', 'underline']))
+    print(termcolor.colored(code, 'green'))
+
+    code_file = io.StringIO(code)
+    fileid = str(uuid.uuid4())
+    code_file.name = fileid + ".r"
+    filepath = self.sandbox.upload_file(code_file)
+    shell_output = self.run_shell(f"Rscript {filepath}")
     return shell_output
 
   def run_shell(self, shell_command: str):
@@ -133,3 +144,21 @@ def EXPERIMENTAL_run_simple_notebook(code,
 
   sandbox.close()
   return stdout, stderr, artifact_files
+
+
+if __name__ == "__main__":
+    # Example usage
+    langsmith_run_id = str(uuid.uuid4())
+    e2b = E2B_class(langsmith_run_id=langsmith_run_id)
+    code = """sayHello <- function(){
+print('hello')
+}
+
+sayHello()
+
+    """
+    print(e2b.run_python_code("print('Hello World')"))
+    print(e2b.run_r_code(code))
+    print(e2b.run_shell("ls -la"))
+    del e2b
+    print("Sandbox closed.")
