b6adbcc55efa92ad4d9d8492dd9ad986be46a005
Kastan Day <kastanvday@gmail.com>
2024-06-06 18:11:48 -0700

    Make cheaper: single worker, multiple threads

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/ai_ta_backend
-ray start --head --num-cpus 6 --object-store-memory 400000000
-exec gunicorn --workers=3 --threads=16 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
+ray start --head --num-cpus 1 --object-store-memory 400000000
+exec gunicorn --workers=1 --threads=10 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
