0a785dfc9fe7333f46536bfb56d60f2f31452f06
Kastan Day <kastanvday@gmail.com>
2024-06-14 12:44:04 -0700

    Print payload so we can test locally

--- ai_ta_backend/main.py
+++ ai_ta_backend/main.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 CORS(app)
 
 # load API keys from globally-availabe .env file
-load_dotenv(dotenv_path='.env', override=True)
+load_dotenv(dotenv_path=".env", override=True)
 
 ray.init()
 
@@ -37,26 +37,26 @@ ray.init()
 #   return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
-@app.route('/coursera', methods=['GET'])
+@app.route("/coursera", methods=["GET"])
 def coursera() -> JSON:
   try:
-    course_name: str = request.args.get('course_name')  # type: ignore
-    coursera_course_name: str = request.args.get('coursera_course_name')  # type: ignore
+    course_name: str = request.args.get("course_name")  # type: ignore
+    coursera_course_name: str = request.args.get("coursera_course_name")  # type: ignore
   except Exception as e:
     print(f"No course name provided: {e}")
 
   ingester = Ingest()
   results = ingester.ingest_coursera(coursera_course_name, course_name)  # type: ignore
   response = jsonify(results)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/github', methods=['GET'])
+@app.route("/github", methods=["GET"])
 def github() -> JSON:
   try:
-    course_name: str = request.args.get('course_name')  # type: ignore
-    github_url: str = request.args.get('github_url')  # type: ignore
+    course_name: str = request.args.get("course_name")  # type: ignore
+    github_url: str = request.args.get("github_url")  # type: ignore
   except Exception as e:
     print(f"No course name provided: {e}")
 
@@ -64,14 +64,14 @@ def github() -> JSON:
   ingester = Ingest()
   results = ingester.ingest_github(github_url, course_name)
   response = jsonify(results)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/delete-entire-course', methods=['GET'])
+@app.route("/delete-entire-course", methods=["GET"])
 def delete_entire_course():
   try:
-    course_name: str = request.args.get('course_name')  # type: ignore
+    course_name: str = request.args.get("course_name")  # type: ignore
     # coursera_course_name: str = request.args.get('coursera_course_name') # type: ignore
   except Exception as e:
     print(f"No course name provided: {e}")
@@ -79,51 +79,51 @@ def delete_entire_course():
   ingester = Ingest()
   results = ingester.delete_entire_course(course_name)  # type: ignore
   response = jsonify(results)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/getTopContexts', methods=['GET'])
+@app.route("/getTopContexts", methods=["GET"])
 def getTopContexts():
   """Get most relevant contexts for a given search query.
-  
-  Return value
 
-  ## GET arguments
-  course name (optional) str
-      A json response with TBD fields.
-  search_query
-  top_n
-  
-  Returns
-  -------
-  JSON
-      A json response with TBD fields.
-  Metadata fileds
-  * pagenumber_or_timestamp
-  * readable_filename
-  * s3_pdf_path
-  
-  Example: 
-  [
-    {
-      'readable_filename': 'Lumetta_notes', 
-      'pagenumber_or_timestamp': 'pg. 19', 
-      's3_pdf_path': '/courses/<course>/Lumetta_notes.pdf', 
-      'text': 'In FSM, we do this...'
-    }, 
-  ]
+    Return value
 
-  Raises
-  ------
-  Exception
-      Testing how exceptions are handled.
-  """
+    ## GET arguments
+    course name (optional) str
+        A json response with TBD fields.
+    search_query
+    top_n
+
+    Returns
+    -------
+    JSON
+        A json response with TBD fields.
+    Metadata fileds
+    * pagenumber_or_timestamp
+    * readable_filename
+    * s3_pdf_path
+
+    Example:
+    [
+      {
+        'readable_filename': 'Lumetta_notes',
+        'pagenumber_or_timestamp': 'pg. 19',
+        's3_pdf_path': '/courses/<course>/Lumetta_notes.pdf',
+        'text': 'In FSM, we do this...'
+      },
+    ]
+
+    Raises
+    ------
+    Exception
+        Testing how exceptions are handled.
+    """
   # todo: best way to handle optional arguments?
   try:
-    course_name: str = request.args.get('course_name')
-    search_query: str = request.args.get('search_query')
-    token_limit: int = request.args.get('token_limit')
+    course_name: str = request.args.get("course_name")
+    search_query: str = request.args.get("search_query")
+    token_limit: int = request.args.get("token_limit")
   except Exception:
     print("No course name provided.")
 
@@ -138,30 +138,30 @@ def getTopContexts():
   found_documents = ingester.getTopContexts(search_query, course_name, token_limit)
 
   response = jsonify(found_documents)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/get_stuffed_prompt', methods=['GET'])
+@app.route("/get_stuffed_prompt", methods=["GET"])
 def get_stuffed_prompt():
   """Get most relevant contexts for a given search query.
-  
-  ## GET arguments
-  course name (optional) str
-      A json response with TBD fields.
-  search_query
-  top_n
-  
-  Returns
-  -------
-    String
-    
-  """
+
+    ## GET arguments
+    course name (optional) str
+        A json response with TBD fields.
+    search_query
+    top_n
+
+    Returns
+    -------
+      String
+
+    """
   # todo: best way to handle optional arguments?
   try:
-    course_name: str = request.args.get('course_name')
-    search_query: str = request.args.get('search_query')
-    token_limit: int = request.args.get('token_limit')
+    course_name: str = request.args.get("course_name")
+    search_query: str = request.args.get("search_query")
+    token_limit: int = request.args.get("token_limit")
   except Exception:
     print("No course name provided.")
 
@@ -177,122 +177,120 @@ def get_stuffed_prompt():
   prompt = ingester.get_stuffed_prompt(search_query, course_name, token_limit)
 
   response = jsonify(prompt)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/ingest', methods=['GET'])
+@app.route("/ingest", methods=["GET"])
 def ingest():
-  """Recursively ingests anything from S3 filepath and below. 
-  Pass a s3_paths filepath (not URL) into our S3 bucket.
-  
-  Ingests all files, not just PDFs. 
-  
-  args:
-    s3_paths: str | List[str]
+  """Recursively ingests anything from S3 filepath and below.
+    Pass a s3_paths filepath (not URL) into our S3 bucket.
 
-  Returns:
-      str: Success or Failure message. Failure message if any failures. TODO: email on failure.
-  """
+    Ingests all files, not just PDFs.
+
+    args:
+      s3_paths: str | List[str]
+
+    Returns:
+        str: Success or Failure message. Failure message if any failures. TODO: email on failure.
+    """
 
   print("In /ingest")
 
   ingester = Ingest()
-  s3_paths: List[str] | str = request.args.get('s3_paths')
-  course_name: List[str] | str = request.args.get('course_name')
+  s3_paths: List[str] | str = request.args.get("s3_paths")
+  course_name: List[str] | str = request.args.get("course_name")
   success_fail_dict = ingester.bulk_ingest(s3_paths, course_name)
 
   response = jsonify(success_fail_dict)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/getContextStuffedPrompt', methods=['GET'])
+@app.route("/getContextStuffedPrompt", methods=["GET"])
 def getContextStuffedPrompt():
   """
-  Get a stuffed prompt for a given user question and course name.
-  Args : 
-    search_query (str)
-    course_name (str) : used for metadata filtering
-  Returns : str
-    a very long "stuffed prompt" with question + summaries of 20 most relevant documents.
-  """
+    Get a stuffed prompt for a given user question and course name.
+    Args :
+      search_query (str)
+      course_name (str) : used for metadata filtering
+    Returns : str
+      a very long "stuffed prompt" with question + summaries of 20 most relevant documents.
+    """
   print("In /getContextStuffedPrompt")
 
   ingester = Ingest()
-  search_query: str = str(request.args.get('search_query'))  # type: ignore
-  course_name: str = str(request.args.get('course_name'))  # type: ignore
-  top_n: int = int(request.args.get('top_n'))  # type: ignore
-  top_k_to_search: int = int(request.args.get('top_k_to_search'))  # type: ignore
+  search_query: str = str(request.args.get("search_query"))  # type: ignore
+  course_name: str = str(request.args.get("course_name"))  # type: ignore
+  top_n: int = int(request.args.get("top_n"))  # type: ignore
+  top_k_to_search: int = int(request.args.get("top_k_to_search"))  # type: ignore
 
   start_time = time.monotonic()
   stuffed_prompt = ingester.get_context_stuffed_prompt(search_query, course_name, top_n, top_k_to_search)
   print(f"⏰ Runtime of EXTREME prompt stuffing: {(time.monotonic() - start_time):.2f} seconds")
   response = jsonify({"prompt": stuffed_prompt})
 
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/getAll', methods=['GET'])
+@app.route("/getAll", methods=["GET"])
 def getAll():
-  """Get all course materials based on the course_name
-  """
+  """Get all course materials based on the course_name"""
 
   print("In /getAll")
 
   ingester = Ingest()
-  course_name: List[str] | str = request.args.get('course_name')
+  course_name: List[str] | str = request.args.get("course_name")
   distinct_dicts = ingester.getAll(course_name)
   response = jsonify({"all_s3_paths": distinct_dicts})
 
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-#Write api to delete s3 files for a course
-@app.route('/delete', methods=['DELETE'])
+# Write api to delete s3 files for a course
+@app.route("/delete", methods=["DELETE"])
 def delete():
-  """Delete all course materials based on the course_name
-    """
+  """Delete all course materials based on the course_name"""
 
   print("In /delete")
 
   ingester = Ingest()
-  course_name: List[str] | str = request.args.get('course_name')
-  s3_path: str = request.args.get('s3_path')
+  course_name: List[str] | str = request.args.get("course_name")
+  s3_path: str = request.args.get("s3_path")
   success_or_failure = ingester.delete_data(s3_path, course_name)
   response = jsonify({"outcome": success_or_failure})
 
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/log', methods=['GET'])
+@app.route("/log", methods=["GET"])
 def log():
   """
-  todo
-  """
+    todo
+    """
 
   print("In /log")
 
   ingester = Ingest()
   # course_name: List[str] | str = request.args.get('course_name')
-  success_or_failure = ingester.log_to_arize('course_name', 'test', 'completion')
+  success_or_failure = ingester.log_to_arize("course_name", "test", "completion")
   response = jsonify({"outcome": success_or_failure})
 
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/web-scrape', methods=['GET'])
+@app.route("/web-scrape", methods=["GET"])
 def scrape():
-  url: str = request.args.get('url')
-  max_urls: int = request.args.get('max_urls')
-  max_depth: int = request.args.get('max_depth')
-  timeout: int = request.args.get('timeout')
-  course_name: str = request.args.get('course_name')
-  base_url_bool: str = request.args.get('base_url_on')
+  url: str = request.args.get("url")
+  max_urls: int = request.args.get("max_urls")
+  max_depth: int = request.args.get("max_depth")
+  timeout: int = request.args.get("timeout")
+  course_name: str = request.args.get("course_name")
+  base_url_bool: str = request.args.get("base_url_on")
 
   # print all input params
   print("Web scrape!")
@@ -304,39 +302,41 @@ def scrape():
   success_fail_dict = main_crawler(url, course_name, max_urls, max_depth, timeout, base_url_bool)
 
   response = jsonify(success_fail_dict)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
-@app.route('/mit-download', methods=['GET'])
+@app.route("/mit-download", methods=["GET"])
 def mit_download_course():
-  url: str = request.args.get('url')
-  course_name: str = request.args.get('course_name')
-  local_dir: str = request.args.get('local_dir')
+  url: str = request.args.get("url")
+  course_name: str = request.args.get("course_name")
+  local_dir: str = request.args.get("local_dir")
 
   success_fail = mit_course_download(url, course_name, local_dir)
 
   response = jsonify(success_fail)
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
 
 # TODO: add a way to delete items from course based on base_url
 
 
-@app.route('/', methods=['POST'])  # RUN: $ smee -u https://smee.io/nRnJDGnCbWYUaSGg --port 8000
+@app.route("/", methods=["POST"])  # RUN: $ smee -u https://smee.io/nRnJDGnCbWYUaSGg --port 8000
 # @app.route('/api/webhook', methods=['POST']) # https://flask-ai-ta-backend-pr-34.up.railway.app/api/webhook
 async def webhook():
   """
-  IN PROGRESS: Github App Webhooks (for lil-jr-dev)
-  Wehbook URL to use on my github app (if this route is `/api/webhook`): https://flask-ai-ta-backend-pr-34.up.railway.app/api/webhook
+    IN PROGRESS: Github App Webhooks (for lil-jr-dev)
+    Wehbook URL to use on my github app (if this route is `/api/webhook`): https://flask-ai-ta-backend-pr-34.up.railway.app/api/webhook
 
-  DOCS: 
-  API reference for Webhook objects: https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads#issue_comment
-  WEBHOOK explainer: https://docs.github.com/en/apps/creating-github-apps/registering-a-github-app/using-webhooks-with-github-apps
-  """
+    DOCS:
+    API reference for Webhook objects: https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads#issue_comment
+    WEBHOOK explainer: https://docs.github.com/en/apps/creating-github-apps/registering-a-github-app/using-webhooks-with-github-apps
+    """
 
   payload = request.json
+  print("Payload received: ")
+  print(payload)
   # print(f"{payload}\n","-"*50, "\n")
   # Writing to sample.json
   with open("sample.json", "w") as outfile:
@@ -346,17 +346,17 @@ async def webhook():
 
   await handle_github_event(payload)
 
-  return '', 200
+  return "", 200
 
 
 async def main():
   # await handle_github_event()
-  f = open('UIUC-Chatbot/ai-ta-backend/sample.json')
+  f = open("UIUC-Chatbot/ai-ta-backend/sample.json")
   payload = json.load(f)
   await handle_github_event(payload)
   pass
 
-if __name__ == '__main__':
-  #app.run(debug=True, port=os.getenv("PORT", default=8000))
-  asyncio.run(main())
 
+if __name__ == "__main__":
+  # app.run(debug=True, port=os.getenv("PORT", default=8000))
+  asyncio.run(main())
