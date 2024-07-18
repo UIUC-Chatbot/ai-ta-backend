667e912be98ffc34443fff3418bc7711daee23df
guest <h3293573671@gmail.com>
2024-07-18 20:17:42 +0000

    finished bulk part of guided-retrival. implemented the basic idea and maximum of 5 steps

--- UIUC_Chat/llm-gretrival/guided_retrival.py
+++ UIUC_Chat/llm-gretrival/guided_retrival.py
@@ -1,120 +1,24 @@
-import json
-import os
+from evaluate_chunks import evaluate_chunks, evaluate_chunks_with_step
 
-from dotenv import load_dotenv
-from openai import OpenAI
+# Example chunks, to be updated with the actual retrival
+query = "How many studies in total met inclusion criteria for analysis of FEV 1?"
+# 1, 10 ,15
+chunks = {
+    "DjkRDVj9YwkfmxAFfgXgJ": "Despite reduced resting lung volumes and D LCO , patients with long COVID and dyspnoea have similar physiological response to exercise to healthy subjects. D LCO impairment can marginally explain heterogeneity of complex syndromes such as long COVID. https://bit.ly/40j4aX6",
+    "e7d6C_gDSEQt5fb6rj8Nt": "Studies were identified using the systematic review methods described previously by KOTECHA et al. [4, 13] which followed the Preferred Reporting Items for Systematic Reviews and Meta-analysis (PRISMA) guidelines [14] . Briefly, 86 studies in total met inclusion criteria for analysis of FEV 1 . Although this systematic review was designed to capture studies to answer questions specifically related to FEV 1 , the search criteria were subsequently deemed acceptable to capture appropriately other spirometry measures including FVC, FEV 1 /FVC ratio and FEF 25-75 . Studies were included for this analysis if they fulfilled the following criteria: 1) FEV 1 /FVC reported in survivors of preterm birth (with or without BPD) and those born healthy at term; or if 2) FEV 1 /FVC were reported separately in survivors of preterm birth with and without BPD.",
+    "Fv2taY_bTmimdP-LE8ldk": "Publication bias was observed when comparing Preterm (All) with Term groups subjectively with an asymmetrical distribution noted on funnel plots, and objectively with Egger's test reaching significance ( p<0.01). When preterm groups were separated into those with and without BPD, however, a symmetrical distribution was noted on all funnel plots and Egger's test did not reach significance (supplementary figure S1 ). This could imply that asymmetry seen in the combined preterm group may be due to the heterogeneity of having two different disease populations defined by the presence or absence of BPD."
+}
+outline = {
+    "DjkRDVj9YwkfmxAFfgXgJ": "0: Abstract\n1: ",
+    "e7d6C_gDSEQt5fb6rj8Nt": "0: Abstract\n1: Introduction\n10: Meta-regression\n11: Discussion\n12: \n2: Research questions\n3: Study identification and selection\n4: Publication bias and study quality\n5: Data collection\n6: Data analysis\n7: Study selection and study quality\n8: Publication bias\n9: Meta-analysis",
+    "Fv2taY_bTmimdP-LE8ldk": "0: Abstract\n1: Introduction\n10: Meta-regression\n11: Discussion\n12: \n2: Research questions\n3: Study identification and selection\n4: Publication bias and study quality\n5: Data collection\n6: Data analysis\n7: Study selection and study quality\n8: Publication bias\n9: Meta-analysis"
+}
 
-load_dotenv()
+chunks_to_keep = []
 
-api_key = os.getenv("AZURE_OPENAI_KEY")
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-deployment = os.getenv("DEPLOYMENT")
-api_version = os.getenv("OPENAI_API_VERSION")
-openai_api = os.getenv("OPENAI_API")
+for chunk_id in chunks:
+    chunks = (evaluate_chunks_with_step(query, chunk_id, 0, chunks_to_keep))
+    if chunks is not None:
+        chunks_to_keep.append(chunks)
 
-client = OpenAI(api_key=openai_api)
-
-tools = [{
-    "type": "function",
-    "function": {
-        "name": "keep_current_chunk",
-        "description": "Check if the current chunk of context is relevant to the query",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "keep": {
-                    "type": "boolean",
-                    "description": "Whether to keep the current chunk or not"
-                }
-            },
-            "required": ["keep"]
-        }
-    },
-    "required": True
-}, {
-    "type": "function",
-    "function": {
-        "name": "check_previous_chunk",
-        "description": "Check if the previous chunk of context is relevant to the query",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "check_previous": {
-                    "type": "boolean",
-                    "description": "True if the previous chunk is relevant, False otherwise"
-                }
-            },
-            "required": ["check_previous"]
-        }
-    },
-}, {
-    "type": "function",
-    "function": {
-        "name": "check_next_chunk",
-        "description": "Check if the next chunk of context is relevant to the query",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "check_next": {
-                    "type": "boolean",
-                    "description": "True if the next chunk is relevant, False otherwise"
-                }
-            },
-            "required": ["check_next"]
-        }
-    }
-}, {
-    "type": "function",
-    "function": {
-        "name": "go_to_URL",
-        "description": "Click link or go to URL referenced in chunk.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "go_to_URL": {
-                    "type": "boolean",
-                    "description": "Whether to go to the URL or not"
-                }
-            },
-            "required": ["go_to_URL"]
-        }
-    }
-}]
-
-query = "What is the name of Polly's son?"
-chunks = [
-    "The Blinders' dominance came about from beating rivals, including the 'Sloggers', 'a pugilistic term for someone who could strike a heavy blow in the ring', whom they fought for territory in Birmingham and its surrounding districts.",
-    "Britain is a mixture of despair and hedonism in 1919 in the aftermath of the Great War. Returning soldiers, newly minted revolutions and criminal gangs are fighting for survival in a nation rocked by economic upheaval.",
-    "Polly's son, Michael Gray, was taken away when he was a baby. He was raised by a family in Australia. He was brought back to Birmingham by Polly in season 2."
-]
-
-messages = [{
-    "role":
-        "system",
-    "content": (
-        "You are an expert in information retrieval. Your task is to evaluate the relevance of a given text chunk to a specific research question. "
-        "You have four functions at your disposal: 'keep_current_chunk', 'check_previous_chunk', 'check_next_chunk', and 'go_to_url'. "
-        "Always use 'keep_current_chunk' to determine if the current chunk is relevant. Then, consider using 'check_previous_chunk' or 'check_next_chunk' or 'go_to_url'. "
-        "When using 'check_previous_chunk', if you find previous chunk relevant to the research question, set 'check_previous' to 'True', otherwise false. "
-        "When using 'check_next_chunk', if you find next chunk relevant to the research question, set 'check_next' to 'True', otherwise false. "
-        "Use 'go_to_url' if the chunk suggests checking an external link.")
-}, {
-    "role":
-        "user",
-    "content": (
-        f"Research Question: '{query}'\n\n"
-        f"Current Text Chunk: '{chunks[1]}'\n\n"
-        "Evaluate the relevance of the current chunk to the research question. Determine if the current chunk should be kept. "
-        "Also, decide whether to check the previous chunk by calling 'check_previous_chunk', or the next chunk by calling 'check_next_chunk', or whether to follow an external link using the respective functions. "
-        "Make sure you call other functions and determine if previous or next chunks are relevant to the research question."
-    )
-}]
-
-completion = client.chat.completions.create(
-    model='gpt-4o',
-    messages=messages,
-    tools=tools,
-    # tool_choice={"type": "function", "function": {"name": "keep_current_chunk"}},
-)
-
-print(completion)
+print(chunks_to_keep)
