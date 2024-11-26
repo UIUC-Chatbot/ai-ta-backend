a880735e4b35dc18fbd9236db2b2935ecb5827c9
star-nox <dabholkar.asmita@gmail.com>
2024-11-26 11:38:15 -0600

    added RPC to fetch distinct base_urls

--- ai_ta_backend/utils/update_project_documents.py
+++ ai_ta_backend/utils/update_project_documents.py
@@ -7,34 +7,23 @@ from ai_ta_backend.database.vector import VectorDatabase
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
-
 load_dotenv()
+
+
+# create Supabase client
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_API_KEY")
+supabase_client = create_client(supabase_url, supabase_key)
+
+
 
 def webscrape_documents(project_name: str):
     print(f"Scraping documents for project: {project_name}")
-
-    # get unique base_urls
-    # base_url_df = pd.read_csv(filepath_or_buffer="./cropwizard_base_urls2.csv")
-    # base_urls = base_url_df['base_url'].unique()
-
-    base_urls = [
-    "https://farmdocdaily.illinois.edu/category/areas",
-    "https://extension.illinois.edu/sites/default/files/managing_diseases_2022web.pdf",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/januaryfebruary-2023",
-    "https://farmdocdaily.illinois.edu/2023/08/farm-bill-2023-is-there-bad-medicine-in-base-acres-and-reference-prices.html",
-    "https://ipcm.wisc.edu/wp-content/uploads/sites/54/2022/11/SeasonalGuide_FINAL.pdf",
-    "https://extension.illinois.edu/global/agronomy-handbook",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/septemberoctober-2022",
-    "https://ipcm.wisc.edu/blog/2023/08/wisconsin-datcp-field-notes-aug-10/",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/novemberdecember-2022",
-    "https://ipcm.wisc.edu/wcm/",
-    "https://farmdocdaily.illinois.edu/2023/09/conventional-and-organic-enterprise-net-returns-4.html",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/julyaugust-2023",
-    "https://extension.illinois.edu/sites/default/files/2022_arb_compiled_accessibility_20230131.pdf",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/marchapril-2023-0",
-    "https://extension.illinois.edu/newsletters/illinois-pesticide-review-newsletter/mayjune-2023"
-    ]
-
+    
+    # use RPC to get unique base_urls
+    response = supabase_client.rpc("get_distinct_base_urls", {"p_course_name": project_name}).execute()
+    print(f"Base urls: {response}")
+    base_urls = response.data
     print(f"Total base_urls: {len(base_urls)}")
     
     #webcrawl_url = "https://crawlee.kastan.ai/crawl"
@@ -55,7 +44,7 @@ def webscrape_documents(project_name: str):
         domain = urlparse(base_url).netloc
         print(f"Domains: {domain}")
         payload["params"]["match"] = "http?(s)://" + domain + "/**"
-        response = requests.post(webcrawl_url, json=payload)
+        #response = requests.post(webcrawl_url, json=payload)
         print("Response from crawl: ", response.json())
 
         # print("Payload: ", payload)
