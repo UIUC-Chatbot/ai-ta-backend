7ec04c6f1d39dbe6fd93799b2fad8ff837102797
Kastan Day <kastanvday@gmail.com>
2025-02-20 13:34:00 -0800

    Activate env

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@
 
 # 200 MB object store memory.. necessary to statically allocate or will crash in Railway env restrictions.
 # ray start --head --num-cpus 6 --object-store-memory 300000000
+
+# Activate the virtual environment
+source /opt/venv/bin/activate
+
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/ai_ta_backend
 exec gunicorn --workers=3 --threads=100 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
