7301fb84a4e34c7c5819c369d0c8ebbb4e813282
Kastan Day <kastanvday@gmail.com>
2025-02-20 14:02:25 -0800

    adding uv run before gunicorn

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 # ray start --head --num-cpus 6 --object-store-memory 300000000
 
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/ai_ta_backend
-exec gunicorn --workers=3 --threads=100 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
+exec uv run gunicorn --workers=3 --threads=100 --worker-class=gthread ai_ta_backend.main:app --timeout 1800
